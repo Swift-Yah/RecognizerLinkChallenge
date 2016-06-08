03bdab61fcff4476a4edc2af8329d3fcbc88d6ca
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-06-07 22:45:32 -0300

    Update version of the simulator on test.sh.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-TEST_CMD="xcodebuild -scheme RecognizerLinkChallenge -workspace RecognizerLinkChallenge.playground -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.1' build test"
+TEST_CMD="xcodebuild -scheme RecognizerLinkChallenge -workspace RecognizerLinkChallenge.playground -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test"
 
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
